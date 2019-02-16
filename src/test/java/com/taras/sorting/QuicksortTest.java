c9ad56b9ea5af06182cbf7ac5ce72aaad317e136
Taras Tkhir <tarasthir@gmail.com>
2019-02-16 11:57:11 +0200

    writing some code in test methods

--- src/test/java/com/taras/sorting/QuicksortTest.java
+++ src/test/java/com/taras/sorting/QuicksortTest.java
@@ -11,7 +11,6 @@ import java.util.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class QuicksortTest {
-
     private static long startTime;
 
     private static Quicksort unitForTestingQuicksortClass;
@@ -29,13 +28,16 @@ class QuicksortTest {
 
         unitForTestingQuicksortClass = null;
 
-        System.out.println( "Time " + (System.currentTimeMillis() - startTime) + "Ms");
+        System.out.println("Time " + (System.currentTimeMillis() - startTime) + "Ms");
         System.out.println("End");
     }
 
     @Test()
     void testQuicksortExceptionWhenListIsEmpty() {
 
+        List<Integer> testUnitList = new LinkedList<Integer>();
+
+        assertThrows(RuntimeException.class, () -> unitForTestingQuicksortClass.quicksort(testUnitList));
 
     }
 
@@ -43,21 +45,36 @@ class QuicksortTest {
     @Test
     void testQuicksortIsArrayListReturned() {
 
+        assertEquals(new ArrayList<Integer>(Arrays.asList(1)), unitForTestingQuicksortClass.quicksort(Arrays.asList(1)));
+    }
 
+    @Test
+    void testQuicksortIsNotArrayListReturned() {
+
+        assertNotEquals(new LinkedHashSet<Integer>(Arrays.asList(1)), unitForTestingQuicksortClass.quicksort(Arrays.asList(1)));
     }
 
     @Test
     void testQuicksortLessListSizeThan2() {
-
+        List<Integer> testSizeList = Arrays.asList(1);
+        int inputSize = testSizeList.size();
+        int outputSize = unitForTestingQuicksortClass.quicksort(testSizeList).size();
+        assertEquals(outputSize, inputSize);
     }
 
     @Test
     void testQuicksortIsSortingWrite() {
 
+        assertEquals(new ArrayList<Integer>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)),
+                unitForTestingQuicksortClass.quicksort(new LinkedList<Integer>(Arrays.asList(5, 7, 8, 1, 2, 3, 4, 6, 10, 9))));
+
     }
 
     @Test
     void testQuicksortIsSortingWrong() {
+
+        assertNotEquals(new ArrayList<Integer>(Arrays.asList(50, 25, 75, 100, 125, 150)),
+                unitForTestingQuicksortClass.quicksort(Arrays.asList(50, 25, 75, 100, 125, 150)));
 
     }
 
