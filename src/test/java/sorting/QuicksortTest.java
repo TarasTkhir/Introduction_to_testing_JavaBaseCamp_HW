86137e1c686c0236071682cd4200264beae9f3de
Taras Tkhir <tarasthir@gmail.com>
2019-02-19 21:42:19 +0200

    test to check work of quicksort

--- src/test/java/sorting/QuicksortTest.java
+++ src/test/java/sorting/QuicksortTest.java
@@ -4,8 +4,10 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -16,13 +18,13 @@ public class QuicksortTest {
     @BeforeAll
     static void setUpBeforeClass() throws Exception {
 
-        unit =new Quicksort();
+        unit = new Quicksort();
     }
 
     @AfterAll
     static void tearDownAfterClass() throws Exception {
 
-     unit =null;
+        unit = null;
     }
 
     @Test
@@ -32,40 +34,54 @@ public class QuicksortTest {
     }
 
     @Test
-    void whenListSizeLessThat2ThenReturnSameListConvertedToArrayList(){
+    void whenListSizeLessThat2ThenReturnSameListConvertedToArrayList() {
 
         assertEquals(new ArrayList<Integer>(Arrays.asList(1)), unit.quicksort(Arrays.asList(1)));
     }
-    @Test
-    void whenTwoListOfGreaterAndLessNumbersAndMiddleNumberIsGivenThenReturnListAddedToMiddleNumberAndToList(){
 
-        assertEquals(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10),unit.addListToList(Arrays.asList(1, 2, 3, 4, 5),
+    @Test
+    void whenTwoListOfGreaterAndLessNumbersAndMiddleNumberIsGivenThenReturnListAddedToMiddleNumberAndToList() {
+
+        assertEquals(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10), unit.addListToList(Arrays.asList(1, 2, 3, 4, 5),
                 6, Arrays.asList(7, 8, 9, 10)));
-        assertEquals(Arrays.asList(3,2,1,4,5,6,9,10,8,7),unit.addListToList(Arrays.asList(3, 2, 1, 4, 5),
+        assertEquals(Arrays.asList(3, 2, 1, 4, 5, 6, 9, 10, 8, 7), unit.addListToList(Arrays.asList(3, 2, 1, 4, 5),
                 6, Arrays.asList(9, 10, 8, 7)));
-        assertEquals(Arrays.asList(75,25,50,100,150,125,175),unit.addListToList(Arrays.asList(75, 25, 50),
-                100, Arrays.asList(150,125,175)));
+        assertEquals(Arrays.asList(75, 25, 50, 100, 150, 125, 175), unit.addListToList(Arrays.asList(75, 25, 50),
+                100, Arrays.asList(150, 125, 175)));
     }
 
     @Test
-    void whenListGivenThenReturnIndexOfMiddleNumber(){
+    void whenListGivenThenReturnIndexOfMiddleNumber() {
 
-        assertEquals(2,unit.middleNumberIndex(Arrays.asList(3,5,8,1,9,6)));
-        assertEquals(3,unit.middleNumberIndex(Arrays.asList(3,5,8,1,9,6,7)));
+        assertEquals(2, unit.middleNumberIndex(Arrays.asList(3, 5, 8, 1, 9, 6)));
+        assertEquals(3, unit.middleNumberIndex(Arrays.asList(3, 5, 8, 1, 9, 6, 7)));
     }
 
     @Test
-    void whenListGivenThenReturnListWhereValuesAreLessThanValueOfMidleIndexOrEqualsToIt(){
+    void whenListGivenThenReturnListWhereValuesAreLessThanValueOfMidleIndexOrEqualsToIt() {
 
-        assertEquals(Arrays.asList(5,7,7,8), unit.getLessList(Arrays.asList(5,7,12,9,7,8,10)));
-        assertEquals(Arrays.asList(1,2,3,1), unit.getLessList(Arrays.asList(1,2,3,4,1,6,7)));
+        assertEquals(Arrays.asList(5, 7, 7, 8), unit.getLessList(Arrays.asList(5, 7, 12, 9, 7, 8, 10)));
+        assertEquals(Arrays.asList(1, 2, 3, 1), unit.getLessList(Arrays.asList(1, 2, 3, 4, 1, 6, 7)));
 
     }
+
     @Test
-    void whenListGivenThenReturnListWhereValuesAreGreaterThanValueOfMidleIndex(){
+    void whenListGivenThenReturnListWhereValuesAreGreaterThanValueOfMidleIndex() {
 
-        assertEquals(Arrays.asList(12,10), unit.getGreaterList(Arrays.asList(5,7,12,9,7,8,10)));
-        assertEquals(Arrays.asList(6,7), unit.getGreaterList(Arrays.asList(1,2,3,4,1,6,7)));
+        assertEquals(Arrays.asList(12, 10), unit.getGreaterList(Arrays.asList(5, 7, 12, 9, 7, 8, 10)));
+        assertEquals(Arrays.asList(6, 7), unit.getGreaterList(Arrays.asList(1, 2, 3, 4, 1, 6, 7)));
 
+    }
+
+    @Test
+    void whenListGivenThenReturnSortedByQuicksortArrayList() {
+        assertEquals(new ArrayList<Integer>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)),
+                unit.quicksort(Arrays.asList(5, 6, 1, 4, 8, 9, 7, 2, 3, 8, 10)));
+
+        assertEquals(new ArrayList<Integer>(Arrays.asList(1, 2, 3, 4, 5, 5, 5, 6, 7, 8, 9, 10)),
+                unit.quicksort(new LinkedList<Integer>(Arrays.asList(5, 7, 8, 1, 2, 3, 4, 6, 10, 9, 5, 5))));
+
+        assertEquals(new ArrayList<Integer>(Arrays.asList(25, 50, 75, 100, 125, 150)),
+                unit.quicksort(Arrays.asList(25, 50, 75, 100, 125, 150)));
     }
 }
